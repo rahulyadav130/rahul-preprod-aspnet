5e14887ca924945b9a9e6a656ab50ad7339996c5
Rahul <techforenjoy@outlook.com>
2020-10-10 14:09:03 +0530

    rahul

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace rahul_prepod_frontend1
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="rahul_prepod_frontend1.MvcApplication" Language="C#" %>
